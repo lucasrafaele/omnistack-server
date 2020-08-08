fde7a3d26e44076ae213a218c9edef1a27ae0902
lucas <lucasrafaele@hotmail.com>
2020-08-07 22:51:20 -0300

    Feat: start build routers

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get("/users", (request, response)=>{
-    return response.json("hello")
-})
 app.listen(3000);
 
